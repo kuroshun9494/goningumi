b39996e43b739e75cd6ad6b072b28e292447dee4
kuroshun9494 <shunta9494shunta@gmail.com>
2021-11-09 01:51:06 +0900

    6-6 done

--- lib/add_post_page.dart
+++ lib/add_post_page.dart
@@ -2,27 +2,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:goningumi/provider.dart';
 
 
-
-
-// 投稿画面用Widget
-class AddPostPage extends StatefulWidget {
-  // 引数からユーザー情報を受け取る
-  AddPostPage(this.user);
-  // ユーザー情報
-  final User user;
-
+// ConsumerWidgetでProviderから値を受け渡す
+class AddPostPage extends ConsumerWidget {
   @override
-  _AddPostPageState createState() => _AddPostPageState();
-}
+  Widget build(BuildContext context, ScopedReader watch) {
+    // Providerから値を受け取る
+    final user = watch(userProvider).state!;
+    final messageText = watch(messageTextProvider).state;
 
-class _AddPostPageState extends State<AddPostPage> {
-  // 入力した投稿メッセージ
-  String messageText = '';
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('チャット投稿'),
@@ -33,17 +25,13 @@ class _AddPostPageState extends State<AddPostPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              // 投稿メッセージ入力
               TextFormField(
                 decoration: InputDecoration(labelText: '投稿メッセージ'),
-                // 複数行のテキスト入力
                 keyboardType: TextInputType.multiline,
-                // 最大3行
                 maxLines: 3,
                 onChanged: (String value) {
-                  setState(() {
-                    messageText = value;
-                  });
+                  // Providerから値を更新
+                  context.read(messageTextProvider).state = value;
                 },
               ),
               const SizedBox(height: 8),
@@ -52,19 +40,16 @@ class _AddPostPageState extends State<AddPostPage> {
                 child: ElevatedButton(
                   child: Text('投稿'),
                   onPressed: () async {
-                    final date =
-                    DateTime.now().toLocal().toIso8601String(); // 現在の日時
-                    final email = widget.user.email; // AddPostPage のデータを参照
-                    // 投稿メッセージ用ドキュメント作成
+                    final date = DateTime.now().toLocal().toIso8601String();
+                    final email = user.email;
                     await FirebaseFirestore.instance
-                        .collection('posts') // コレクションID指定
-                        .doc() // ドキュメントID自動生成
+                        .collection('posts')
+                        .doc()
                         .set({
                       'text': messageText,
                       'email': email,
                       'date': date
                     });
-                    // 1つ前の画面に戻る
                     Navigator.of(context).pop();
                   },
                 ),
--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -2,31 +2,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:goningumi/provider.dart';
 import 'add_post_page.dart';
 import 'main.dart';
 
-class ChatPage extends StatelessWidget {
-  // 引数からユーザー情報を受け取れるようにする
-  ChatPage(this.user);
-
-  // ユーザー情報
-  final User user;
-
+// ConsumerWidgetでProviderから値を受け渡す
+class ChatPage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    // Providerから値を受け取る
+    final User user = watch(userProvider).state!;
+    final AsyncValue<QuerySnapshot> asyncPostsQuery = watch(postsQueryProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('チャット'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.logout),
+            icon: Icon(Icons.close),
             onPressed: () async {
-              // ログアウト処理
-              // 内部で保持しているログイン情報等が初期化される
-              // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
               await FirebaseAuth.instance.signOut();
-              // ログイン画面に遷移＋チャット画面を破棄
               await Navigator.of(context).pushReplacement(
                 MaterialPageRoute(builder: (context) {
                   return LoginPage();
@@ -43,47 +40,43 @@ class ChatPage extends StatelessWidget {
             child: Text('ログイン情報：${user.email}'),
           ),
           Expanded(
-            // FutureBuilder
-            // 非同期処理の結果を元にWidgetを作れる
-            child: StreamBuilder<QuerySnapshot>(
-              // 投稿メッセージ一覧を取得（非同期処理）
-              // 投稿日時でソート
-              stream: FirebaseFirestore.instance
-                  .collection('posts')
-                  .orderBy('date')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                // データが取得できた場合
-                if (snapshot.hasData) {
-                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
-                  // 取得した投稿メッセージ一覧を元にリスト表示
-                  return ListView(
-                    children: documents.map((document) {
-                      return Card(
-                        child: ListTile(
-                          title: Text(document['text']),
-                          subtitle: Text(document['email']),
-                          // 自分の投稿メッセージの場合は削除ボタンを表示
-                          trailing: document['email'] == user.email
-                              ? IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () async {
-                              // 投稿メッセージのドキュメントを削除
-                              await FirebaseFirestore.instance
-                                  .collection('posts')
-                                  .doc(document.id)
-                                  .delete();
-                            },
-                          )
-                              : null,
-                        ),
-                      );
-                    }).toList(),
-                  );
-                }
-                // データが読込中の場合
+            // StreamProviderから受け取った値は .when() で状態に応じて出し分けできる
+            child: asyncPostsQuery.when(
+              // 値が取得できたとき
+              data: (QuerySnapshot query) {
+                return ListView(
+                  children: query.docs.map((document) {
+                    return Card(
+                      child: ListTile(
+                        title: Text(document['text']),
+                        subtitle: Text(document['email']),
+                        trailing: document['email'] == user.email
+                            ? IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () async {
+                            // 投稿メッセージのドキュメントを削除
+                            await FirebaseFirestore.instance
+                                .collection('posts')
+                                .doc(document.id)
+                                .delete();
+                          },
+                        )
+                            : null,
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
+              // 値が読込中のとき
+              loading: () {
                 return Center(
                   child: Text('読込中...'),
+                );
+              },
+              // 値の取得に失敗したとき
+              error: (e, stackTrace) {
+                return Center(
+                  child: Text(e.toString()),
                 );
               },
             ),
@@ -93,11 +86,9 @@ class ChatPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () async {
-          // 投稿画面に遷移
           await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
-              // 引数からユーザー情報を渡す
-              return AddPostPage(user);
+              return AddPostPage();
             }),
           );
         },
