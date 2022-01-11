7fe310ee98c9d69ce2aa84964e2e2e4680eed0ca
mizukoshi <ryuhei123012@gmail.com>
2022-01-11 21:35:27 +0900

    listtile化

--- lib/list_channel.dart
+++ lib/list_channel.dart
@@ -22,23 +22,29 @@ class ListChannel extends StatefulWidget {
 }
 
 
-
-Widget buttonFunction(BuildContext context) {
-  return FloatingActionButton(onPressed: () {
-    Navigator.push(context, MaterialPageRoute(
-      // （2） 実際に表示するページ(ウィジェット)を指定する
-        builder: (context) => FirstPage()
-    ));
-  });
-}
-
-
-Widget chatTitle(String title) {
-  return Text(
-    title,
-    style: TextStyle(fontSize: 24),
+Widget channellistTile(BuildContext context, String title, Color color) {
+  return ListTile(
+    title: Text(title),
+    contentPadding: EdgeInsets.all(15.0),
+    tileColor: color,
+    leading: IconButton(
+      icon: Icon(Icons.people),
+      onPressed: (){
+        Navigator.push(context, MaterialPageRoute(
+          // （2） 実際に表示するページ(ウィジェット)を指定する
+            builder: (context) => FirstPage()
+      ));
+    },
+    ),
+    trailing: Icon(Icons.more_vert),
   );
 }
+
+
+
+
+
+
 
 
 class EachChannelTransition extends State<ListChannel> {
@@ -46,25 +52,41 @@ class EachChannelTransition extends State<ListChannel> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo'),
+        title: Text('チャンネル一覧'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children:<Widget> [
-            buttonFunction(context),
-            chatTitle("乃木坂"),
-            buttonFunction(context),
-            chatTitle("野球"),
-            buttonFunction(context),
-            chatTitle("音楽"),
-            buttonFunction(context),
-            chatTitle("乃木坂"),
-            buttonFunction(context),
-            chatTitle("競プロ"),
-          ],
+        child: Container(
+          margin: EdgeInsets.all(5),
+          // padding: EdgeInsets.all(5),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children:<Widget> [
+              channellistTile(context, "アウトドア", Colors.red.withOpacity(0.5)),
+              const SizedBox(height: 5),
+              channellistTile(context, "漫画", Colors.yellow.withOpacity(0.5)),
+              const SizedBox(height: 5),
+              channellistTile(context, "音楽", Colors.blue.withOpacity(0.5)),
+              const SizedBox(height: 5),
+              channellistTile(context, "野球", Colors.green.withOpacity(0.5)),
+              const SizedBox(height: 5),
+              channellistTile(context, "競プロ", Colors.pink.withOpacity(0.5)),
+            ],
+          ),
         )
       ),
     );
   }
 }
+
+/*
+buttonFunction(context),
+chatTitle("乃木坂"),
+buttonFunction(context),
+chatTitle("野球"),
+buttonFunction(context),
+chatTitle("音楽"),
+buttonFunction(context),
+chatTitle("乃木坂"),
+buttonFunction(context),
+chatTitle("競プロ"),
+*/
